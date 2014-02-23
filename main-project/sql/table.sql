6f646879aa6628f767017bbf602e3ef839a29741
jorgechavez77 <jorgechavez77@gmail.com>
2014-02-22 19:13:23 -0500

    Add SQL scripts

---
+++ main-project/sql/table.sql
@@ -0,0 +1,15 @@
+/*
+SQLyog Community v11.31 (64 bit)
+MySQL - 5.1.48-community 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `fabricacion` (
+	`id` bigint (20),
+	`numero_serie` varchar (300),
+	`codigo` bigint (10),
+	`modelo` varchar (300),
+	`fecha_fabricacion` timestamp ,
+	`proveedor` varchar (300)
+); 
